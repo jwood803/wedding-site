61ddba007a4005d2f81738a074238affc75f0a05
Jon Wood <woodjon@gmail.com>
2016-08-26 12:41:58 -0400

    Trying to get collapsed header going...

--- app/wedding.component.ts
+++ app/wedding.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CollapseDirective } from 'ng2-bootstrap';
 
 @Component({
   moduleId: module.id,
   selector: 'wedding-app',
-  templateUrl: `app.component.html`,
-  styles: ['#body { text-align: center; }']
+  templateUrl: 'app.component.html',
+  styles: ['#body { text-align: center; }'],
+  directives: []
 })
 export class WeddingComponent { 
     names = "Jonathan & Janah";
