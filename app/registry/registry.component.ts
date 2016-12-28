b78d90a68c06d7d19777ba5454489325642f1838
Jon Wood <woodjon@gmail.com>
2016-12-28 06:51:58 -0500

    Remove old code

--- app/registry/registry.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'registry',
-  templateUrl: `registry.component.html`
-})
-export class RegistryComponent { }
