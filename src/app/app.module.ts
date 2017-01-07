8aaa982fc7abaa24558cc53ebca371edf5c4561e
Jon Wood <woodjon@gmail.com>
2017-01-07 12:15:37 -0500

    Start adding firebase to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegistryComponent } from './registry/registry.component';
 import { RsvpComponent } from './rsvp/rsvp.component';
 import { routing } from './app.routing';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2joa4kZf8ftWbLCsBtHsmdsaVe021T58",
+  authDomain: "wedding-1483790611518.firebaseapp.com",
+  databaseURL: "https://wedding-1483790611518.firebaseio.com",
+  storageBucket: "wedding-1483790611518.appspot.com",
+  messagingSenderId: "880711324362"
+};
 
 @NgModule({
   declarations: [
@@ -20,6 +28,7 @@ import { routing } from './app.routing';
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     routing
   ],
   providers: [],
