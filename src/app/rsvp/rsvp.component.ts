69a0d36b7f23c62c169dcdf9db8c74972ba25667
Jon Wood <woodjon@gmail.com>
2017-01-08 13:32:02 -0500

    Get RSVP form starting to work

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -6,8 +6,30 @@ import { RsvpService } from './rsvp.service';
 })
 export class RsvpComponent {
   rsvps: any;
+  inviteStatus: string;
+  guestType: string;
+  isGuestBringingDate: boolean;
 
   constructor(private rsvpService: RsvpService) {
-    this.rsvps = this.rsvpService.getData();
-  }  
+    this.inviteStatus = "going";
+    this.guestType = "self";
+  }
+
+  onGuestTypeChange(value) {
+    console.log("Guest type changed");
+    console.log(value);
+
+    if(value === "selfPlusDate") {
+      this.isGuestBringingDate = true;
+    }
+  }
+
+  onInviteStatusChange(value) {
+    console.log("Status changed");
+    console.log(value);
+  }
+
+  onSubmit(form) {
+    console.log(form);
+  }
 }
