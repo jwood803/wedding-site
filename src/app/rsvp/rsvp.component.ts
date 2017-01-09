6e16120560c4207d9f95867df7c079404f8a774a
Jon Wood <woodjon@gmail.com>
2017-01-08 19:19:44 -0500

    Add toast notifications

--- src/app/rsvp/rsvp.component.ts
+++ src/app/rsvp/rsvp.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+import { NotificationsService } from 'angular2-notifications';
 import { RsvpService } from './rsvp.service';
-import { AngularFire } from 'angularfire2';
 
 @Component({
   templateUrl: `./rsvp.component.html`
@@ -11,8 +11,23 @@ export class RsvpComponent {
   guestType: string;
   isGuestBringingDate: boolean;
 
-  constructor(private rsvpService: RsvpService, private angularFire: AngularFire) {
-    this.rsvps = this.angularFire.database.list('/rsvps');
+  public notificationOptions = {
+        timeOut: 3000,
+        lastOnBottom: false,
+        clickToClose: true,
+        maxLength: 0,
+        maxStack: 2,
+        showProgressBar: false,
+        pauseOnHover: true,
+        preventDuplicates: false,
+        preventLastDuplicates: 'visible',
+        rtl: false,
+        animate: 'scale',
+        position: ['right', 'top']
+    };
+
+
+  constructor(private rsvpService: RsvpService, private notificationService: NotificationsService) {
     this.inviteStatus = "going";
     this.guestType = "self";
   }
@@ -22,11 +37,20 @@ export class RsvpComponent {
       this.isGuestBringingDate = true;
     }
   }
-  
+
   onSubmit(form) {
     console.log(form);
+    let successMessage = "Your RSVP has been sent! Can't wait to see you there!";
+
+    if(form.inviteStatus !== "going") {
+      successMessage = "Sorry you can't make it :("
+    }
 
     this.rsvpService.postRsvp(form)
-      .subscribe(data => console.log(data));
+      .subscribe(
+        data => console.log(data),
+        err => this.notificationService.error("Oh, no!", "Your RSVP didn't make it :( Please try again."),
+        () => this.notificationService.success("RSVP Sent!", successMessage)
+      );
   }
 }
