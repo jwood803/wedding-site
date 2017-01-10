eb6d00d25bf02aa36f55d24b9d1c5a9c9514d729
Jon Wood <woodjon@gmail.com>
2017-01-10 16:37:40 -0500

    Add multiple guests to RSVP

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   templateUrl: './registry.component.html'
 })
-export class RegistryComponent { }
+export class RegistryComponent { 
+  isProdMode: boolean;
+
+  constructor() {
+    this.isProdMode = environment.production;
+  }
+}
